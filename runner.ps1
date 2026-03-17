6fbb48723cf8b74790c39975a48aa095c45ac17f
Your Name <you@example.com>
2026-03-18 01:36:57 +0300

    add admin mode

--- runner.ps1
+++ runner.ps1
@@ -1,3 +1,13 @@
+$isAdmin = ([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    Start-Process powershell.exe `
+        -Verb RunAs `
+        -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    exit
+}
 $projectPath = "C:\Path\To\Your\Project"
 $activatePath = ".\venv\Scripts\Activate.ps1"
 
